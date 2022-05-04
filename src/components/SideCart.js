292d8a7a05c682251eac877ca812396dee761770
Adrien Callioni <adrien.callioni@gmail.com>
2022-05-04 17:02:30 +0200

    doing offer

---
+++ src/components/SideCart.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SideCart = (props) => {
+  return (
+    <aside className="side-cart">
+      <div></div>
+      <div>
+        <p></p>
+        <p></p>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </aside>
+  );
+};
+
+export default SideCart;
--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -1,9 +1,51 @@
 import React from "react";
-import { useState, useEffect, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import SideCart from "../components/SideCart";
+
+import axios from "axios";
 
 const Offer = () => {
-  const { params } = useParams(); // _id
-  return <div>Offer</div>;
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState();
+
+  const { _id } = useParams(); // _id
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios(
+        `https://lereacteur-vinted-api.herokuapp.com/offer/${_id}`
+      );
+
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {!isLoading ? (
+        <main className="offer-holder">
+          <div className="offer-images-holder">
+            {data.product_pictures.map((img, index) => {
+              return (
+                <div className="offer-images">
+                  <img
+                    className="offer-image-solo"
+                    src={img.secure_url}
+                    alt="product"
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <SideCart {...data} />
+        </main>
+      ) : null}
+    </>
+  );
 };
 
 export default Offer;
